3145734f5dac3d4c05a7b6b4ba0718834192bd9b
Jahmal Lewis <Jahmal23@gmail.com>
2016-08-24 11:10:39 -0400

    fix for rails / angular CSRF fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  # This is handled by the gem - angular_rails_csrf
   #protect_from_forgery with: :exception
 end
