15a1e6f1c3b028d54e545a7f1a6280f020ddfeab
Jahmal Lewis <Jahmal23@gmail.com>
2016-09-05 17:25:32 -0500

    testing progress

--- spec/controllers/tacos_controller_spec.rb
+++ spec/controllers/tacos_controller_spec.rb
@@ -1,3 +1,20 @@
-
+require 'rails_helper'
 require 'spec_helper'
 
+RSpec.describe TacosController, :type => :controller do
+  let!(:tacos) { FactoryGirl.create_list(:taco, 4) }
+
+  describe '#index' do
+
+    before(:each) { get :index }
+
+    it 'assigns all tacos to @tacos' do
+      expect(assigns(:tacos)).to match_array tacos
+    end
+
+    it 'is successful' do
+      expect(response).to be_success
+    end
+  end
+
+end
---
+++ spec/factories/tacos.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :taco do
+    notes "new taco"
+  end
+
+end
